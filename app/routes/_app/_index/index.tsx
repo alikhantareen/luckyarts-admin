cd6a4a731304d47a759eeae13d0c0f8072dda6ac
alikhantareen <creditableboy@gmail.com>
2023-04-01 02:59:42 +0500

    add dashboard ui changes

--- app/routes/_app/_index/index.tsx
+++ app/routes/_app/_index/index.tsx
@@ -3,25 +3,95 @@ import maintenanceImage from "../../../assets/maintenance.svg";
 
 export default function Index() {
   return (
-    <div className="flex flex-col justify-center items-center px-6 mx-auto h-[calc(100vh-74px)] xl:px-0 dark:bg-gray-900">
-      <div className="block mb-5 md:max-w-md">
-        <img src={maintenanceImage} alt="Maintenance" />
+    <div className="p-4">
+      <h1 className="text-xl font-semibold text-gray-900 md:text-2xl dark:text-white">
+        Dashboard
+      </h1>
+      <div className="bg-white rounded-lg border-2 border-slate-300 p-8 mt-5 flex flex-col gap-5 w-fit">
+        <div className="flex flex-col md:flex-row gap-5 p-2">
+          <div className="flex items-center gap-3">
+            <p className="font-bold">From</p>
+            <input
+              className="w-full"
+              type="date"
+              name="initialdate"
+              id="initialdate"
+            />
+          </div>
+          <div className="flex items-center gap-8">
+            <p className="font-bold">To</p>
+            <input
+              className="w-full"
+              type="date"
+              name="initialdate"
+              id="initialdate"
+            />
+          </div>
+        </div>
+        <div>
+          <h1 className="text-xl font-semibold text-gray-900 md:text-2xl dark:text-white">
+            Invoices
+          </h1>
+          <div className="flex flex-col gap-5 md:flex-row mt-5">
+            <a
+              href="#"
+              className="flex flex-col justify-center items-center text-slate-50 w-full h-40 md:w-48 md:h-32 p-6 bg-[#dd2822] rounded-lg shadow hover:bg-[#f40901] dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 hover:-translate-y-1 duration-200 drop-shadow-xl"
+            >
+              <p className="text-lg font-semibold">Unpaid</p>
+              <p className="text-2xl font-semibold">23</p>
+              <p className="text-xs font-semibold">Total Amount Due</p>
+              <p className="text-xl font-semibold">Rs. 17000</p>
+            </a>
+            <a
+              href="#"
+              className="flex flex-col justify-center items-center text-slate-50 w-full h-40 md:w-48 md:h-32 p-6 bg-[#f3c41a] rounded-lg shadow hover:bg-[#FFCB06] dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 hover:-translate-y-1 duration-200 drop-shadow-xl"
+            >
+              <p className="text-lg font-semibold">Partial Paid</p>
+              <p className="text-2xl font-semibold">23</p>
+              <p className="text-xs font-semibold">Total Amount Due</p>
+              <p className="text-xl font-semibold">Rs. 17000</p>
+            </a>
+            <a
+              href="#"
+              className="flex flex-col justify-center items-center text-slate-50 w-full h-40 md:w-48 md:h-32 p-6 bg-[#379d37] rounded-lg shadow hover:bg-[#2ab52a] dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 hover:-translate-y-1 duration-200 drop-shadow-xl"
+            >
+              <p className="text-lg font-semibold">Full Paid</p>
+              <p className="text-2xl font-semibold">23</p>
+              <p className="text-xs font-semibold">Total Amount Due</p>
+              <p className="text-xl font-semibold">Rs. 17000</p>
+            </a>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-xl font-semibold text-gray-900 md:text-2xl dark:text-white">
+            Orders
+          </h1>
+          <div className="flex flex-col gap-5 md:flex-row mt-5">
+            <a
+              href="#"
+              className="flex flex-col justify-center items-center text-slate-50 w-full h-40 md:w-48 md:h-32 p-6 bg-[#dd2822] rounded-lg shadow hover:bg-[#f40901] dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 hover:-translate-y-1 duration-200 drop-shadow-xl"
+            >
+              <p className="text-lg font-semibold">Pending</p>
+              <p className="text-2xl font-semibold">15</p>
+            </a>
+            <a
+              href="#"
+              className="flex flex-col justify-center items-center text-slate-50 w-full h-40 md:w-48 md:h-32 p-6 bg-[#f3c41a] rounded-lg shadow hover:bg-[#FFCB06] dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 hover:-translate-y-1 duration-200 drop-shadow-xl"
+            >
+              <p className="text-lg font-semibold">In Progress</p>
+              <p className="text-2xl font-semibold">9</p>
+            </a>
+            <a
+              href="#"
+              className="flex flex-col justify-center items-center text-slate-50 w-full h-40 md:w-48 md:h-32 p-6 bg-[#379d37] rounded-lg shadow hover:bg-[#2ab52a] dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 hover:-translate-y-1 duration-200 drop-shadow-xl"
+            >
+              <p className="text-lg font-semibold">Complete</p>
+              <p className="text-2xl font-semibold">10</p>
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="text-center xl:max-w-4xl">
-        <h1 className="mb-3 text-2xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl dark:text-white">
-          Work In Progress
-        </h1>
-        <p className="mb-5 text-base font-normal text-gray-500 md:text-lg dark:text-gray-400">
-          Hang out with us a bit more as we're still building this one out.
-          Please check out the invoices page meanwhile
-        </p>
-        <NavLink
-          to="/invoices"
-          className="w-full sm:w-fit text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-          Go to Invoices
-        </NavLink>
-      </div>
+      <div></div>
     </div>
   );
 }
